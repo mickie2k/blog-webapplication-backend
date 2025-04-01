e9d1b348b081992b76f02ac7bf53b89869930a48
Garaiiiii <toriyakikuributa@gmail.com>
2025-04-01 22:04:53 +0700

    add validate comment

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,6 +1,17 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
+
 export class CreateCommentDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(500)
     comment: string;
+
+    @IsNotEmpty()
+    @IsString()
     userid: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     blogid: number;
 }
     
