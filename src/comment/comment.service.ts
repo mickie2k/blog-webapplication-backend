db77599545989c24ee94971409e29e3451bcd889
Garaiiiii <toriyakikuributa@gmail.com>
2025-04-01 22:33:42 +0700

    push delete comment api and find comment by blog id

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -4,6 +4,7 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 import { DRIZZLE } from 'src/drizzle/drizzle.module';
 import { DrizzleDB } from 'src/drizzle/types/drizzletype';
 import * as schema from 'src/drizzle/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class CommentService {
@@ -19,19 +20,20 @@ export class CommentService {
     }
   }
 
-  findAll() {
-    return `This action returns all comment`;
+  async findByBlogId(id: number) {
+    const result = await this.db.query.comment.findMany({
+      where: eq(schema.comment.blogid, id),
+    });
+    return result;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
-
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+  async remove(id: number) {
+    try{
+      const deleteComment = await this.db.delete(schema.comment).where(eq(schema.comment.id, id));
+      return deleteComment;
+    }catch (error) {
+      console.log(error)
+      throw new Error('Failed to delete blog');
+    }
   }
 }
